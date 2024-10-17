5aa3c295ec7b83828aa48a33664efda21136b4e6
TTPL-LNVE15-0229 <swapnilranmale2003@gmail.com>
2024-10-17 11:19:17 +0530

    basic form created and get the on webpage)

--- src/app/forms/forms.component.ts
+++ src/app/forms/forms.component.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
+import { CommonModule, NgFor } from "@angular/common";
+import { Component, NgModule } from "@angular/core";
+import { FormsModule, NgForm, NgModel } from "@angular/forms";
 
 @Component({
-  selector: 'app-forms',
+  selector: "app-forms",
   standalone: true,
-  imports: [],
-  templateUrl: './forms.component.html',
-  styleUrl: './forms.component.css'
+  imports: [CommonModule, FormsModule, NgFor],
+  templateUrl: "./forms.component.html",
+  styleUrl: "./forms.component.css",
 })
 export class FormsComponent {
-
+  udata: any = {};
+  onSubmit(data: NgForm) {
+    console.log(data);
+    this.udata = data;
+  }
 }
