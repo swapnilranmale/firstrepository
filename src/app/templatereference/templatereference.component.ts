147ce91711e8bef34925260c207345e0b81c5d79
TTPL-LNVE15-0229 <swapnilranmale2003@gmail.com>
2024-10-17 16:34:54 +0530

    created custom pipe

--- src/app/templatereference/templatereference.component.ts
+++ src/app/templatereference/templatereference.component.ts
@@ -1,14 +1,22 @@
-import { Component } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { Component, Pipe } from "@angular/core";
+import { UsdinrPipe } from "../pipes/usdinr.pipe";
 
 @Component({
   selector: "app-templatereference",
   standalone: true,
-  imports: [],
+  imports: [CommonModule, UsdinrPipe],
   templateUrl: "./templatereference.component.html",
   styleUrl: "./templatereference.component.css",
 })
 export class TemplatereferenceComponent {
+  nameoftitle = "thinkitive tech";
   getData(item: any) {
     console.log(item);
   }
+  today = Date.now();
+
+  users = [{ name: "swapnil", email: "swapnilranmale@thinkitive.com" }];
+
+  carNumber = 25000.39028;
 }
