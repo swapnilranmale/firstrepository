2ec7c716d3cbbf6b0f01d600f71311a3886cadfa
TTPL-LNVE15-0229 <swapnilranmale2003@gmail.com>
2024-10-17 13:15:04 +0530

    send data from parent component to child component

---
+++ src/app/child/child.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input } from "@angular/core";
+
+@Component({
+  selector: "app-child",
+  standalone: true,
+  imports: [],
+  templateUrl: "./child.component.html",
+  styleUrl: "./child.component.css",
+})
+export class ChildComponent {
+  @Input() item = 0;
+  @Input() user = "abc";
+}
